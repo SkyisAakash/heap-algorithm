208a49d9aa983f9ff5fa913b7285b30517c18fe6
Aakash Sarang <sarangaakash@gmail.com>
2018-07-04 23:16:06 -0700

    Add heapify method

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,6 +1,32 @@
 require_relative "heap"
+require'byebug'
 
 class Array
   def heap_sort!
+    self.heapify!
+  end
+
+  def heapify
+    (1..length-1).each do |heap_line|
+      # debugger
+      correct = false
+      target_idx = heap_line
+      while correct === false
+        break if target_idx == 0
+        parent = BinaryMinHeap.parent_index(target_idx)
+        correct = true
+        if parent
+          if self[parent] < self[target_idx]
+            self[parent], self[target_idx] = self[target_idx], self[parent]
+            target_idx = parent
+            correct = false
+          end
+        end
+      end
+    end
   end
 end
+
+a = [6,4,5,7,8]
+a.heapify
+puts(a)
